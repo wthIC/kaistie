7e5c7c5926061b276403e5d7ab8ad2257f001d9e
Murad Eynizada <eynizadamurad@gmail.com>
2022-11-28 04:54:29 +0900

    small refactor

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class ChatScreen extends StatelessWidget {
@@ -109,71 +108,6 @@ class ChatScreen extends StatelessWidget {
           ),
         ),
       ),
-    );
-  }
-}
-
-class _CustomButton extends StatelessWidget {
-  const _CustomButton({
-    Key? key,
-    required this.onTap,
-    required this.buttonText,
-    required this.color,
-    required this.textColor,
-    this.disabled = false,
-    this.loading = false,
-  }) : super(key: key);
-
-  final Function() onTap;
-  final String buttonText;
-  final bool disabled;
-  final Color? color;
-  final Color textColor;
-  final Color disabledColor = const Color.fromRGBO(144, 164, 174, 1);
-  final bool loading;
-
-  @override
-  Widget build(BuildContext context) {
-    final TextTheme textTheme = Theme.of(context).textTheme;
-    final ColorScheme colorScheme = Theme.of(context).colorScheme;
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Container(
-          height: 48.0,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8.0),
-            color: color,
-          ),
-          child: Material(
-            color: Colors.transparent,
-            child: InkWell(
-              splashColor: disabledColor,
-              highlightColor: disabledColor,
-              borderRadius: BorderRadius.circular(8.0),
-              onTap: disabled ? null : onTap,
-              child: SizedBox(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 14.0),
-                  child: Center(
-                    child: loading
-                        ? const SpinKitThreeBounce(
-                            color: Colors.white,
-                            size: 20,
-                          )
-                        : Text(
-                            buttonText,
-                            style: textTheme.button!.copyWith(color: textColor),
-                            textAlign: TextAlign.center,
-                          ),
-                  ),
-                ),
-                // height: 40.0,
-              ),
-            ),
-          ),
-        ),
-      ],
     );
   }
 }
