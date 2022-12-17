338369a1deea57e7bb53c074012f2561a8a4cf2c
Murad Eynizada <eynizadamurad@gmail.com>
2022-12-18 03:30:17 +0900

    correct self

--- lib/screens/my_page_self_screen.dart
+++ lib/screens/my_page_self_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
@@ -31,142 +32,171 @@ class MyPageSelf extends StatelessWidget {
           body: Padding(
             padding: const EdgeInsets.all(24.0),
             child: SizedBox.expand(
-              child: Column(
-                children: [
-                  SvgPicture.asset('assets/image_placeholder.svg'),
-                  const SizedBox(height: 24.0),
-                  const Text(
-                    'Hi, Sasha Murad 👋',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      fontWeight: FontWeight.w700,
-                    ),
-                  ),
-                  const SizedBox(height: 24.0),
-                  Row(
-                    children: const [
-                      Text(
-                        'Account',
-                        style: TextStyle(
-                          fontSize: 12.0,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 8.0),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      Text(
-                        'Email',
-                        style: TextStyle(
-                          fontSize: 14.0,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      Text(
-                        'noza@reinatt.com',
-                        style: TextStyle(
-                          fontSize: 14.0,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 24.0),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      Text(
-                        'Phone number',
-                        style: TextStyle(
-                          fontSize: 14.0,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      Text(
-                        '+82 10-4200-6969',
-                        style: TextStyle(
-                          fontSize: 14.0,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 24.0),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: const [
-                      Text(
-                        'Student ID',
-                        style: TextStyle(
-                          fontSize: 14.0,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      Text(
-                        '20242069',
-                        style: TextStyle(
-                          fontSize: 14.0,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ],
-                  ),
-                  const SizedBox(height: 24.0),
-                  const Text(
-                    'Bio',
-                    style: TextStyle(
-                      fontSize: 14.0,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  const SizedBox(height: 8.0),
-                  Container(
-                    width: double.infinity,
-                    decoration: BoxDecoration(
-                      color: Colors.white,
-                      border: Border.all(
-                        color: Colors.grey,
-                        width: 1.0,
-                      ),
-                      borderRadius: BorderRadius.circular(4.0),
-                    ),
-                    child: const Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: Text(
-                        'I am looking for a roommate to hang out and study with!',
-                        style: TextStyle(
-                          fontSize: 14.0,
-                          fontWeight: FontWeight.w400,
-                        ),
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 24.0),
-                  _CustomButton(
-                    textColor: Colors.white,
-                    onTap: () {},
-                    color: colorScheme.primary,
-                    buttonText: 'Take Roommate Compatibility Test',
-                  ),
-                  const SizedBox(height: 24.0),
-                  _CustomButton(
-                    textColor: Colors.black,
-                    onTap: () {
-                      FirebaseAuth.instance.signOut();
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => SignInScreen(),
-                        ),
-                      );
-                    },
-                    color: const Color(0xFFCBD2E0),
-                    buttonText: 'Sign Out',
-                  ),
-                ],
+              child: StreamBuilder(
+                stream:
+                    FirebaseFirestore.instance.collection('users').snapshots(),
+                builder:
+                    (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+                  return ListView.builder(
+                      itemCount: streamSnapshot.data?.docs.length ?? 0,
+                      itemBuilder: (ctx, index) {
+                        final User user = FirebaseAuth.instance.currentUser!;
+                        print(user.uid);
+
+                        final docUser = FirebaseFirestore.instance
+                            .collection('users')
+                            .doc(user.uid);
+
+                        return FirebaseAuth.instance.currentUser!.uid ==
+                                streamSnapshot.data!.docs[index]['uid']
+                            ? Column(
+                                children: [
+                                  SvgPicture.asset(
+                                      'assets/image_placeholder.svg'),
+                                  const SizedBox(height: 24.0),
+                                  Text(
+                                    'Hi, ${streamSnapshot.data!.docs[index]['fullName']} 👋',
+                                    style: const TextStyle(
+                                      fontSize: 20.0,
+                                      fontWeight: FontWeight.w700,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 24.0),
+                                  Row(
+                                    children: const [
+                                      Text(
+                                        'Account',
+                                        style: TextStyle(
+                                          fontSize: 12.0,
+                                          fontWeight: FontWeight.w400,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(height: 8.0),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      const Text(
+                                        'Email',
+                                        style: TextStyle(
+                                          fontSize: 14.0,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                      Text(
+                                        streamSnapshot.data!.docs[index]
+                                            ['email'],
+                                        style: const TextStyle(
+                                          fontSize: 14.0,
+                                          fontWeight: FontWeight.w400,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(height: 24.0),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      const Text(
+                                        'Phone number',
+                                        style: TextStyle(
+                                          fontSize: 14.0,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                      Text(
+                                        streamSnapshot.data!.docs[index]
+                                            ['phoneNumber'],
+                                        style: const TextStyle(
+                                          fontSize: 14.0,
+                                          fontWeight: FontWeight.w400,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(height: 24.0),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      const Text(
+                                        'Student ID',
+                                        style: TextStyle(
+                                          fontSize: 14.0,
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                      ),
+                                      Text(
+                                        streamSnapshot.data!.docs[index]
+                                            ['studentID'],
+                                        style: const TextStyle(
+                                          fontSize: 14.0,
+                                          fontWeight: FontWeight.w400,
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                  const SizedBox(height: 24.0),
+                                  const Text(
+                                    'Bio',
+                                    style: TextStyle(
+                                      fontSize: 14.0,
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                  ),
+                                  const SizedBox(height: 8.0),
+                                  Container(
+                                    width: double.infinity,
+                                    decoration: BoxDecoration(
+                                      color: Colors.white,
+                                      border: Border.all(
+                                        color: Colors.grey,
+                                        width: 1.0,
+                                      ),
+                                      borderRadius: BorderRadius.circular(4.0),
+                                    ),
+                                    child: const Padding(
+                                      padding: EdgeInsets.all(8.0),
+                                      child: Text(
+                                        'I am looking for a roommate to hang out and study with!',
+                                        style: TextStyle(
+                                          fontSize: 14.0,
+                                          fontWeight: FontWeight.w400,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  const SizedBox(height: 24.0),
+                                  _CustomButton(
+                                    textColor: Colors.white,
+                                    onTap: () {},
+                                    color: colorScheme.primary,
+                                    buttonText:
+                                        'Take Roommate Compatibility Test',
+                                  ),
+                                  const SizedBox(height: 24.0),
+                                  _CustomButton(
+                                    textColor: Colors.black,
+                                    onTap: () {
+                                      FirebaseAuth.instance.signOut();
+                                      Navigator.pushReplacement(
+                                        context,
+                                        MaterialPageRoute(
+                                          builder: (context) => SignInScreen(),
+                                        ),
+                                      );
+                                    },
+                                    color: const Color(0xFFCBD2E0),
+                                    buttonText: 'Sign Out',
+                                  ),
+                                ],
+                              )
+                            : const SizedBox();
+                      });
+                },
               ),
             ),
           ),
